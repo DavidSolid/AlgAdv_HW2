62d7618679f12470d3c87700325582f01f85c290
Enrico Cancelli <cancelli1997@gmail.com>
2020-05-05 13:20:06 +0200

    Creata struttura generale del progetto

---
+++ graph_structures/AdjacencyMatrix.h
@@ -0,0 +1,12 @@
+//
+// Created by Enrico on 05/05/2020.
+//
+
+#ifndef ALGADV_HW2_ADJACENCYMATRIX_H
+#define ALGADV_HW2_ADJACENCYMATRIX_H
+
+class AdjacencyMatrix{
+    //
+};
+
+#endif //ALGADV_HW2_ADJACENCYMATRIX_H
