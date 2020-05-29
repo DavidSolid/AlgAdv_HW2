eb19df5a3a70bf0eed3855ab54a6ceb2ccabf3a6
AlessandroPegoraro <spampego@gmail.com>
2020-05-30 00:46:10 +0200

    Corretto Cheapest insertion, implementazione corretta da risultati peggiori di implementazione errata

--- algorithms/cheapest_insertion.h
+++ algorithms/cheapest_insertion.h
@@ -15,8 +15,8 @@ std::vector<unsigned int> cheapest_insertion(Matrix<T> w){
 
     //find j who minimize w(0,j)
     T find_min = (T)INT_MAX;
-    unsigned int find_node = -1;
-    for(unsigned int j = 1; j<w.sizeY(); ++j){
+    int find_node = -1;
+    for(int j = 1; j<w.sizeY(); ++j){
         if(w.at(0,j) < find_min) {
             find_min = w.at(0, j);
             find_node = j;
@@ -33,13 +33,13 @@ std::vector<unsigned int> cheapest_insertion(Matrix<T> w){
         find_node = -1;
         unsigned int temp_pos = INT_MAX;
         //we always know that 0 is already inside C
-        for(unsigned int k = 1; k<w.sizeY(); ++k){
+        for(int k = 1; k<w.sizeY(); ++k){
             //check if j is already inside C
             if(!L[k]){
                 //C.size()-1 because C is a circuit so C[0] == C[end]
                 for(unsigned int i = 0; i<C.size()-1; ++i) {
                     if((w.at(C[i],k) + w.at(C[i+1],k) - w.at(C[i],C[i+1])) < find_min){
-                        find_min = w.at(i,k) + w.at(C[i],k) - w.at(i,C[i]);
+                        find_min = w.at(C[i],k) + w.at(C[i+1],k) - w.at(C[i],C[i+1]);
                         find_node = k;
                         temp_pos = i+1;
                     }
--- main.cpp
+++ main.cpp
@@ -12,13 +12,48 @@
 
 
 int main() {
+
+    /*
+    Parser myParser = Parser();
+    Graph_Data data = Parser::parse("..\\dataset\\burma14.tsp");
+
+    std::cout << data.dim << std::endl;
+
+    std::cout << data.name << std::endl;
+
+    std::cout << "is geo?: " << (data.ty == coord_type::geo) << std::endl;
+
+    std::cout << "is cartesian?: " << (data.ty == coord_type::cartesian) << std::endl;
+
+    Matrix mat(data.get_weights());
+
+    auto cycle_2approx = preorder_2approx(mat);
+
+    auto cycle_cheapest = cheapest_insertion(mat);
+
+    auto cycle_held_karp = held_karp(mat);
+
+    double res_held_karp = 0;
+    double res_2approx = 0;
+    double res_cheapest = 0;
+    for(unsigned int i = 0; i < mat.sizeY(); ++i){
+        res_held_karp += mat.at(cycle_held_karp[i], cycle_held_karp[i+1]);
+        res_2approx += mat.at(cycle_2approx[i], cycle_2approx[i+1]);
+        res_cheapest += mat.at(cycle_cheapest[i], cycle_cheapest[i+1]);
+    }
+
+    std::cout << "held_karp : " << res_held_karp << std::endl;
+    std::cout << "2approx   : " << res_2approx << std::endl;
+    std::cout << "cheapest  : " << res_cheapest << std::endl << std::endl;
+    */
+
     std::string path = "..\\dataset";
     Parser myParser = Parser();
     for (const auto &entry : std::filesystem::directory_iterator(path)) {
         Graph_Data data = Parser::parse(entry.path());
-        //Graph_Data data = Parser::parse("..\\dataset\\burma14.tsp");
 
         std::cout << data.dim << std::endl;
+
         std::cout << data.name << std::endl;
 
         std::cout << "is geo?: " << (data.ty == coord_type::geo) << std::endl;
