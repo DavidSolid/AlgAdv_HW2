764b8d15d8ebea5d343a22bdc0477780ee625c5b
PcPortatile <cancelli1997@gmail.com>
2020-06-03 00:24:45 +0200

    Sistemata tabella, fatto 2approx, iniziata analisi (1 di 2)

---
+++ relazione/2approx.cpp
@@ -0,0 +1,17 @@
+std::vector<unsigned int> preorder(unsigned int f_elem, const Tree_t& tree){
+    std::vector<unsigned int> out;
+
+    out.emplace_back(f_elem);
+    for(unsigned int i: tree[f_elem]){
+        auto part = preorder(i, tree);
+        out.insert(out.end(), part.begin(), part.end());
+    }
+    return out;
+}
+
+std::vector<unsigned int> preorder_2approx(Matrix<T> w){
+    Tree_t mst = Prim(w);
+    std::vector<unsigned int> chain = preorder(0, mst);
+    chain.emplace_back(0);
+    return chain;
+}
